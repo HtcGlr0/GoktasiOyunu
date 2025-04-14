7e50cbb8f3a3aa3e5bd1d7ddeaf76ee9c4240024
HtcGlr0 <haticeguler40551@gmail.com>
2025-04-14 17:57:45 +0300

    x ve y ekseni için pozisyonlar, göktaşları için yönleri ve göktaşının genişiği ile yüksekliği değişkenler ile tanımlandı.

---
+++ JGoktasi.java
@@ -0,0 +1,12 @@
+import java.awt.Polygon;
+
+class JGoktasi extends Polygon {
+    int uSolXpoz, uSolYPoz;
+
+    int xYonu = 1;
+    int yYonu = 1;
+
+    int genislik = JOyunalani.oyunAlaniGenislik;
+    int yukseklik = JOyunalani.oyunAlaniYukseklik;
+    
+}
