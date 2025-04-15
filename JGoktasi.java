dcf614870b617fd7ea7d0f92612275861e790831
HtcGlr0 <haticeguler40551@gmail.com>
2025-04-15 17:14:15 +0300

    Çokgenler tanımlandı

--- JGoktasi.java
+++ JGoktasi.java
@@ -8,5 +8,12 @@ class JGoktasi extends Polygon {
 
     int genislik = JOyunalani.oyunAlaniGenislik;
     int yukseklik = JOyunalani.oyunAlaniYukseklik;
+
+    int [] cokgenXArray , cokgenYArray;
+    
+    public static int[] hbCokgenXArray = {10,17,26,34,27,36,26,14,8,1,5,1,10};
+    public static int[] hbCokgenYArray = {10,17,26,34,27,36,26,14,8,1,5,1,10};
+
+
     
 }
