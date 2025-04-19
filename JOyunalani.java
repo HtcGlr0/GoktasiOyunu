9db730dedf9ad5c12af33c97f284e9b9621b21cf
HtcGlr0 <haticeguler40551@gmail.com>
2025-04-19 14:15:08 +0300

    JOyunalani adında yeni bir dosya oluşturuldu ve kütüphaneler tanımlandı

---
+++ JOyunalani.java
@@ -0,0 +1,18 @@
+/*
+import java.awt.BorderLayout;
+import java.awt.color.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import javax.swing.JComponent; 
+*/
+
+import javax.swing.JFrame;
+
+
+public class JOyunalani extends JFrame {
+    
+}
