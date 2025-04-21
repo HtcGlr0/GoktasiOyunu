744bb688506af97e5bb7de5f5852502a3368682c
HtcGlr0 <haticeguler40551@gmail.com>
2025-04-21 20:34:55 +0300

    Kontrol yapıldı

--- JOyunalani.java
+++ JOyunalani.java
@@ -1,4 +1,4 @@
-/*
+
 import java.awt.BorderLayout;
 import java.awt.color.*;
 import java.awt.Graphics;
@@ -7,12 +7,10 @@ import java.awt.RenderingHints;
 import java.util.ArrayList;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import javax.swing.JComponent; 
-*/
+import javax.swing.JComponent;
 
 import javax.swing.JFrame;
 
-
 public class JOyunalani extends JFrame {
-    
+
 }
