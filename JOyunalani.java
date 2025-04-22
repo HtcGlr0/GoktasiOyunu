6b09f99a12d94cdc6b096d83b76f14710911a502
HtcGlr0 <haticeguler40551@gmail.com>
2025-04-22 12:08:49 +0300

    Constructur oluşturuldu ve eklemeler yapıldı

--- JOyunalani.java
+++ JOyunalani.java
@@ -1,4 +1,3 @@
-
 import java.awt.BorderLayout;
 import java.awt.color.*;
 import java.awt.Graphics;
@@ -7,10 +6,28 @@ import java.awt.RenderingHints;
 import java.util.ArrayList;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
-import javax.swing.JComponent;
-
+import javax.swing.JComponent; 
 import javax.swing.JFrame;
 
-public class JOyunalani extends JFrame {
 
+public class JOyunalani extends JFrame {
+    public static int oyunAlaniGenislik = 1000;
+    public static int oyunAlaniYukseklik = 800;
+
+    public static void main(String[] args) {
+        new JOyunalani();
+    }
+
+    public JOyunalani(){
+        this.setSize(oyunAlaniGenislik, oyunAlaniYukseklik);
+        this.setTitle("ProPhpHocam Java Uzay Oyunu");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.add(oyunPanel, BorderLayout.CENTER);
+
+        ScheduledThreadPoolExecutor calistir = new ScheduledThreadPoolExecutor(5);
+        calistir.scheduleAtFixedRate(new EkraniTemizle(this), 0L, 20L, TimeUnit.MILLISECONDS);
+        this.setVisible(true); 
+    }
+    
 }
