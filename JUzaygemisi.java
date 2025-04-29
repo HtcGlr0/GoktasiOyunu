e0e50954ea2803170e03e7a8f36e53f9c00fbc9a
HtcGlr0 <haticeguler40551@gmail.com>
2025-04-29 21:55:12 +0300

    Tanımlamalar yapıldı

---
+++ JUzaygemisi.java
@@ -0,0 +1,20 @@
+import java.awt.Polygon;
+
+@SuppressWarnings("serial")
+public class JUzaygemisi extends Polygon {
+    int uSolXpoz = 500;
+    int uSolYPoz = 400;
+
+    int xYonu = 0;
+    int yYonu = 0;
+
+    int genislik = JOyunalani.oyunAlaniGenislik;
+    int yukseklik = JOyunalani.oyunAlaniYukseklik;
+
+    public static int[] hbCokgenXArray = {500, 527, 500, 508, 500};
+    public static int[] hbCokgenYArray = {400, 415, 430, 415, 400};
+    
+
+
+
+}
