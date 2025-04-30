d660a69efe3cc2aaa85cc93fc7e9998653624169
HtcGlr0 <haticeguler40551@gmail.com>
2025-04-30 19:20:26 +0300

    Constructurlar oluşturuldu

--- JUzaygemisi.java
+++ JUzaygemisi.java
@@ -11,8 +11,25 @@ public class JUzaygemisi extends Polygon {
     int genislik = JOyunalani.oyunAlaniGenislik;
     int yukseklik = JOyunalani.oyunAlaniYukseklik;
 
-    public static int[] hbCokgenXArray = {500, 527, 500, 508, 500};
-    public static int[] hbCokgenYArray = {400, 415, 430, 415, 400};
+    public static int[] cokgenXArray = {500, 527, 500, 508, 500};
+    public static int[] cokgenYArray = {400, 415, 430, 415, 400};
+
+    public JUzaygemisi(){
+        super(cokgenXArray, cokgenYArray, 5);
+    }
+
+    public void hareketEt(){
+        int uSolXpoz = super.xpoints[0];
+        int uSolYpoz = super.ypoints[0];
+
+        if( uSolXpoz < 0  || (uSolXpoz+25) > genislik) xYonu = -xYonu;
+        if( uSolXpoz < 0  || (uSolXpoz+50) > yukseklik) yYonu = -yYonu;
+
+        for(int i=0; i < super.xpoints.length; i++){
+            super.xpoints[i] += xYonu;
+            super.ypoints[i] += yYonu;
+        }
+    }
     
 
 
