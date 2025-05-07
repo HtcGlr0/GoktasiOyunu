8a387806e6c32f98d253f60bdcb15e8fe4edf54d
HtcGlr0 <haticeguler40551@gmail.com>
2025-05-07 17:26:40 +0300

    Uzay gemisi yeniden oluşturulmaya başlandı

--- JUzaygemisi.java
+++ JUzaygemisi.java
@@ -2,42 +2,16 @@ import java.awt.Polygon;
 
 @SuppressWarnings("serial")
 public class JUzaygemisi extends Polygon {
-    int uSolXpoz = 500;
-    int uSolYPoz = 400;
 
-    int xYonu = 0;
-    int yYonu = 0;
+    private double xHiz = 0, yHiz = 0;
 
-    int genislik = JOyunalani.oyunAlaniGenislik;
-    int yukseklik = JOyunalani.oyunAlaniYukseklik;
+    int oAGenislik = JOyunalani.oyunAlaniGenislik;
+    int oAYukseklik = JOyunalani.oyunAlaniYukseklik;
+
+    private double ortalaX = oAGenislik/2, ortalaY/2 ;
+    
 
     public static int[] cokgenXArray = {-13, 14, -13, -5, -13};
     public static int[] cokgenYArray = {-15, 0, 15, 0, -15};
-
-    static int yonAcisi = 0;
-
-    public JUzaygemisi(){
-        super(cokgenXArray, cokgenYArray, 5);
-    }
-
-    public void hareketEt(){
-        /* 
-        int uSolXpoz = super.xpoints[0];
-        int uSolYpoz = super.ypoints[0];
-
-        if( uSolXpoz < 0  || (uSolXpoz+25) > genislik) xYonu = -xYonu;
-        if( uSolXpoz < 0  || (uSolXpoz+50) > yukseklik) yYonu = -yYonu;
-
-        for(int i=0; i < super.xpoints.length; i++){
-            super.xpoints[i] += xYonu;
-            super.ypoints[i] += yYonu;
-        } */
-
-        super.xpoints = JUzaygemisi.cokgenXArray;
-        super.ypoints = JUzaygemisi.cokgenYArray;
-    }
-    
-
-
 
 }
