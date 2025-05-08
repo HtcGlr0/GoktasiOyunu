9a21f8db3d802bc4108a3c04965d181e0a6149c4
HtcGlr0 <haticeguler40551@gmail.com>
2025-05-08 10:41:20 +0300

    Uzay gemisi oluşturulmaya devam ediliyor

--- JUzaygemisi.java
+++ JUzaygemisi.java
@@ -8,10 +8,21 @@ public class JUzaygemisi extends Polygon {
     int oAGenislik = JOyunalani.oyunAlaniGenislik;
     int oAYukseklik = JOyunalani.oyunAlaniYukseklik;
 
-    private double ortalaX = oAGenislik/2, ortalaY/2 ;
+    private double ortalaX = oAGenislik/2, ortalaY = oAYukseklik/2 ;
     
 
     public static int[] cokgenXArray = {-13, 14, -13, -5, -13};
     public static int[] cokgenYArray = {-15, 0, 15, 0, -15};
+
+    private int gemiGenislik = 27, gemiYukseklil = 30;
+
+    private double uSolXPoz = getXOrtala() + this.cokgenXArray[0];
+    private double uSolYPoz = getYOrtala() + this.cokgenYArray[0];
+
+    private double yonAcisi = 0, hareketAcisi = 0;
+
+    public JUzaygemisi(){
+        super(cokgenXArray, cokgenYArray, 5);
+    }
 
 }
