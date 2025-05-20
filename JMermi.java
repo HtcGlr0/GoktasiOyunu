84cd16ccd4255d84d14b14e4d3f9b83378616f4e
HtcGlr0 <haticeguler40551@gmail.com>
2025-05-20 21:40:34 +0300

    Mermi oluşturulmaya devam ediliyor

--- JMermi.java
+++ JMermi.java
@@ -20,6 +20,33 @@ public class JMermi extends Polygon {
 
     private double xHiz = 5, yHiz = 5;
 
+    public JMermi(double ortalaX, double ortalaY, double hareketAcisi){
+        super(cokgenXArray, cokgenYArray, 5);
+        this.ortalaX = ortalaX;
+        this.ortalaY = ortalaY;
+        this.hareketAcisi = hareketAcisi;
 
+        this.setXHiz(this.mermiXHareketAcisi(hareketAcisi)*10);
+        this.setYHiz(this.mermiYHareketAcisi(hareketAcisi)*10);
+
+    }
+
+    public double getXOrtala(){return ortalaX;}
+    public double getYOrtala(){return ortalaY;}
+
+    public void setXOrtala(double ortalaX){this.ortalaX = ortalaX;}
+    public void setYOrtala(double ortalaY){this.ortalaY = ortalaY;}
+
+    public void xPozisyonDegistir(double Arttir){this.ortalaX += Arttir;}
+    public void yPozisyonDegistir(double Arttir){this.ortalaY += Arttir;}
+
+    public double getXHiz(){return xHiz;}
+    public double getYHiz(){return yHiz;}
+
+    public void getXHiz(double xHiz){this.xHiz = xHiz;}
+    public void getYHiz(double yHiz){this.yHiz = yHiz;}
+
+    public int getGenislik(){return mermiGenislik;}
+    public int getYukseklik(){return mermiYukseklik;}
     
 }
